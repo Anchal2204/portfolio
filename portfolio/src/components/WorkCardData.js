aa09d8ce0e3dcee38b383768af66f1487bbcb957
Anchal2204 <anchal.mittan22@gmail.com>
2022-11-28 20:23:20 +0530

    first commit

---
+++ portfolio/src/components/WorkCardData.js
@@ -0,0 +1,34 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+const ProjectCardData =[
+    {
+        imgsrc: pro1,
+        
+        title: "Ecommerce Website Design",
+        
+        text:"E-commerce is the activity of electronically buying or selling of products on online services or over the Internet. E-commerce draws on technologies such as mobile commerce, electronic funds transfer, supply chain management, Internet marketing, online transaction processing, electronic data interchange, inventory management systems, and automated data collection systems. E-commerce is in turn driven by the technological advances of the semiconductor industry, and is the largest sector of the electronics industry.",
+        
+        view:"https://www.youtube.com/watch?v=P8YuWEkTeuE",
+    },
+    {
+        imgsrc: pro2,
+        
+        title: "Weather Forecast",
+        
+        text:"Get the India weather forecast. Access hourly, 10 day and 15 day forecasts along with up to the minute reports",
+        
+        view:"https://www.youtube.com/watch?v=P8YuWEkTeuE",
+    },
+    {
+        imgsrc: pro3,
+        
+        title: "Currency Converter",
+        
+        text:"We use the mid-market rate for our Converter. This is for informational purposes only. You won’t receive this rate when sending money.",
+        
+        view:"https://www.youtube.com/watch?v=P8YuWEkTeuE",
+    },
+    
+];
+export default ProjectCardData;
